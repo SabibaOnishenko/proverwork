04bf4fe591aee148cd4897fb336e5bc1a4eda642
Sabina <sanas1982@mail.ru>
2022-08-09 17:15:05 +0300

    добавлен поиск слов, в которых меньше или ранов 3 символа

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ for (int i = 0; i < array.Length; i++)
 {
     Console.WriteLine(array[i]);
 }
+
+Console.WriteLine();
+Console.WriteLine("Отображаем все слова, в которых 3 или менее символов");
+ for (int i = 0; i < array.Length; i++)
+ {
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]);
+    }
+ }
